1dcc13a76ba52edb1f840bbaeae362f28bd285c8
aungkaungkhant-nssh <akkgit0909@gmail.com>
2022-12-27 10:06:36 +0630

    fix

--- controllers/user/userAuthController.js
+++ controllers/user/userAuthController.js
@@ -59,11 +59,12 @@ exports.userLogin = async (req,res)=>{
 exports.emailVerify = async(req,res)=>{
     try{
         const user  = await User.findById(req.params.id);
-        if(!user) return res.status(400).json({message:"1Invalid Link"});
+        if(!user) return res.status(400).json({message:"Invalid Link"});
         const token  = await Token.findOne({userId:user._id,token:req.params.token});
-        if(!token) return res.status(400).json({message:"2Invalid Link"});
+        if(!token) return res.status(400).json({message:"Invalid Link"});
         await User.updateOne({_id:user._id,verified:true});
         await Token.findByIdAndRemove(token._id);
+        res.status(200).json({message:"Email Verify Success",data:{emailVerified:true}})
     }catch(err){
         console.log(err)
         res.status(500).json({message:"Something went wrong"});
